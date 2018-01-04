87be33368fb92e2d09afceff5581c71ccdd974a3
PJ Frias <peejfrias@gmail.com>
2018-01-04 04:24:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -9,7 +9,7 @@ def prime?(num)
     false
   end
   test_range = (2..num-1).to_a
-  if (test_range.any? {|test| num % test == 0)
+  if (test_range.any? {|test| num % test == 0})
     false
   else
     true
